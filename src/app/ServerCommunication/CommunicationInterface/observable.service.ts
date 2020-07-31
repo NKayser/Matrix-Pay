dbcf403a7c4a5365d0b300f0639d573006cb983b
Marion <uwxkt@student.kit.edu>
2020-07-31 13:30:11 +0200

    method signatures (first version)

--- src/app/ServerCommunication/CommunicationInterface/observable.service.ts
+++ src/app/ServerCommunication/CommunicationInterface/observable.service.ts
@@ -1,9 +1,53 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {ObservableInterface} from './observableInterface';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ObservableService {
+export class ObservableService implements ObservableInterface {
 
-  constructor() { }
+  constructor() {
+  }
+
+  // inside the Observable construction, listen to matrix events and pass the data via the string parameter of subscriber.next()
+  getGroupsObservable(): Observable<string> {
+    return new Observable(subscriber => subscriber.next('someString'));
+  }
+
+  getBalancesObservable(): Observable<string> {
+    return new Observable(subscriber => subscriber.next('someString'));
+  }
+
+  getRecommendationsObservable(): Observable<string> {
+    return new Observable(subscriber => subscriber.next('someString'));
+  }
+
+  getGroupMembershipObservable(): Observable<string> {
+    return new Observable(subscriber => subscriber.next('someString'));
+  }
+
+  getNewTransactionObservable(): Observable<string> {
+    return new Observable(subscriber => subscriber.next('someString'));
+  }
+
+  getModifiedTransactionObservable(): Observable<string> {
+    return new Observable(subscriber => subscriber.next('someString'));
+  }
+
+  getSettingsLanguageObservable(): Observable<string> {
+    return new Observable(subscriber => subscriber.next('someString'));
+  }
+
+  getSettingsCurrencyObservable(): Observable<string> {
+    return new Observable(subscriber => subscriber.next('someString'));
+  }
+
+  getOldTransactionsObservable(): Observable<string> {
+    return new Observable(subscriber => subscriber.next('someString'));
+  }
+
+  getOldGroupCreationActivityObservable(): Observable<string> {
+    return new Observable(subscriber => subscriber.next('someString'));
+  }
 }
--- src/app/Update/emergent-data-update.service.ts
+++ src/app/Update/emergent-data-update.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {ObservableInterface} from '../ServerCommunication/CommunicationInterface/observableInterface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmergentDataUpdateService {
+  private observables: ObservableInterface;
 
-  constructor() { }
+  // injection of DataModelService is missing
+  constructor(observables: ObservableInterface) {
+    this.observables = observables;
+  }
+
+  private updateBalances(groupID: string, balances: number[], participantIds: string[]): void {
+    return null;
+  }
+
+  private updateRecommendationsForUserPaybacks(groupID: string, amounts: number[], receiverIds: string[]): void {
+    return null;
+  }
 }
