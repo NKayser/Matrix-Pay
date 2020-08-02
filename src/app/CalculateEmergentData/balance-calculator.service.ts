c5650f20efdccdf0510e009821fbfaa7924cdd4a
Marion <uwxkt@student.kit.edu>
2020-08-02 11:20:21 +0200

    Methodensignaturen

--- src/app/CalculateEmergentData/balance-calculator.service.ts
+++ src/app/CalculateEmergentData/balance-calculator.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import {ProblemInstance} from './problemInstance';
+// as soon as available, import Transaction from DataModel
+// import {Transaction} from './DataModel/Transaction'
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,9 @@ import { Injectable } from '@angular/core';
 export class BalanceCalculatorService {
 
   constructor() { }
+
+  // to be implemented: calculation of balances
+  calculateBalances(oldBalances: Map<string, number>, lastDate: Date /*,transactions: Transaction[*/): ProblemInstance {
+    return new ProblemInstance(['a', 'b'], [1, 2]);
+  }
 }
