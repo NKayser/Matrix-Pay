4ddd9e44f84dfd58bc708cddcdaa581573622c95
Markus Rüggeberg <markus.rueggeberg@student.kit.de>
2020-08-02 16:29:30 +0200

    alle Signaturen bis auf DataModelService geschrieben. Triviale Methoden imlementiert. Nicht-implementierte Methoden mit TODOs annotiert.

--- src/app/DataModel/Group/Contact.ts
+++ src/app/DataModel/Group/Contact.ts
@@ -1,2 +1,21 @@
-class Contact {
+export class Contact {
+  private _contactId: string; //not yet const
+  private _name: string;
+
+  public constructor (contactId: string, name: string) {
+    this._contactId = contactId;
+    this._name = name;
+  }
+
+  get contactId(): string {
+    return this._contactId;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
 }
--- src/app/DataModel/Group/Recommendation.ts
+++ src/app/DataModel/Group/Recommendation.ts
@@ -1,2 +1,26 @@
-class Recommendation {
+import {Group} from "./Group";
+import {AtomarChange} from "./AtomarChange";
+
+export class Recommendation {
+  private readonly _group: Group;
+  private readonly _payer: AtomarChange;
+  private readonly _recipiant: AtomarChange;
+
+  public constructor(group: Group, payer: AtomarChange, recipiant: AtomarChange) {
+    this._group = group;
+    this._payer = payer;
+    this._recipiant = recipiant;
+  }
+
+  get group(): Group {
+    return this._group;
+  }
+
+  get payer(): AtomarChange {
+    return this._payer;
+  }
+
+  get recipiant(): AtomarChange {
+    return this._recipiant;
+  }
 }
