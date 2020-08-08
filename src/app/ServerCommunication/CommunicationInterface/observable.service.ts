3d5ff3ef0fd3e90ec836c074fed670108fa36179
Marion <uwxkt@student.kit.edu>
2020-08-08 18:58:52 +0200

    Add detection of paybacks

--- src/app/ServerCommunication/CommunicationInterface/observable.service.ts
+++ src/app/ServerCommunication/CommunicationInterface/observable.service.ts
@@ -50,6 +50,8 @@ export class ObservableService implements ObservableInterface {
     // these two lines are temporary (test)
     let domain = this.matrixClient.getDomain();
     console.log(domain);
+
+    // fires whenever accoint data changes
     this.matrixClient.on('accountData', (event, oldEvent) => {
       // console.log('got account data change' + event.getType());
       if (event.getType() === 'currency') {
@@ -57,6 +59,7 @@ export class ObservableService implements ObservableInterface {
         this.settingsCurrencyObservable.next({currency: event.getContent().currency});
       }
     });
+
     // Fires whenever invited to a room or joining a room
     // TODO: check whether client joined, check whether it is a MatrixPay room
     this.matrixClient.on('Room', room => {
@@ -67,6 +70,18 @@ export class ObservableService implements ObservableInterface {
       console.log(currency);
       // TODO: get missing attributes from matrix
       this.groupsObservable.next({groupId, groupName, currency: 'EURO', userIds: ['a', 'b'], userNames: ['Karl', 'Sophie'], isLeave: false});
+    });
+
+    // Fires whenever the timeline in a room is updated
+    this.matrixClient.on("Room.timeline",
+      (event, room, toStartOfTimeline, removed, data) => {
+      console.log('got a timeline change. event type: '  + event.getType());
+      // do we need this check?
+      if (!toStartOfTimeline && data.liveEvent) {
+        if (event.getType() === "payback") {
+          console.log('got payback. name: ' + event.getContent()["name"]);
+        }
+      }
     });
   }
 
