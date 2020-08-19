3a51b93a9e3dedfd3e1e900cee7348c31e57fbbe
Paul Ferdinand Heinen <uzpjs@student.kit.edu>
2020-08-19 18:07:10 +0200

    Tests for GroupBalance GroupTransaction History Settings

--- src/app/ViewModel/history/history.component.spec.ts
+++ src/app/ViewModel/history/history.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HistoryComponent } from './history.component';
+import {MatDialog} from '@angular/material/dialog';
+import {MockDialog} from '../_mockServices/MockDialog';
+import {MatrixBasicDataService} from '../../ServerCommunication/CommunicationInterface/matrix-basic-data.service';
+import {DataModelService} from '../../DataModel/data-model.service';
+import {Group} from '../../DataModel/Group/Group';
+import {Currency} from '../../DataModel/Utils/Currency';
+import {Activity} from '../../DataModel/Group/Activity';
 
 describe('HistoryComponent', () => {
   let component: HistoryComponent;
@@ -11,15 +18,36 @@ describe('HistoryComponent', () => {
       declarations: [ HistoryComponent ]
     })
     .compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(HistoryComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  }));
 
-  /*it('should create', () => {
-    expect(component).toBeTruthy();
-  });*/
+  it('change test', () => {
+
+    const g1 = new Group('g1', 'name_g1', Currency.USD);
+    const g2 = new Group('g2', 'name_g2', Currency.USD);
+
+    const a1 = new Activity(null, null, null, null);
+    const a2 = new Activity(null, null, null, null);
+    const a3 = new Activity(null, null, null, null);
+    const a4 = new Activity(null, null, null, null);
+    g1.addActivity(a1);
+    g1.addActivity(a2);
+    g2.addActivity(a3);
+    g2.addActivity(a4);
+
+    console.log(g1.activities);
+
+    component.group = g1;
+    component.ngOnChanges();
+    console.log(component.activities);
+    expect(component.activities).toEqual([a1, a2]);
+
+    component.group = g2;
+    component.ngOnChanges();
+    console.log(component.activities);
+    expect(component.activities).toEqual([a3, a4]);
+
+  });
 });
