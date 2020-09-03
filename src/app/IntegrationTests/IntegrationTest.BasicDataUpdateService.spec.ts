add5b249b9a23ebe68d6ccf9e7642f838196be45
Markus Rüggeberg <markus.rueggeberg@student.kit.edu>
2020-09-03 08:56:14 +0200

    started writing SystemTest for UpdateService, DataModel

--- src/app/IntegrationTests/IntegrationTest.BasicDataUpdateService.spec.ts
+++ src/app/IntegrationTests/IntegrationTest.BasicDataUpdateService.spec.ts
@@ -1,15 +1,51 @@
-import { TestBed } from '@angular/core/testing';
-import { BasicDataUpdateService } from '../Update/basic-data-update.service';
+import {BasicDataUpdateService} from '../Update/basic-data-update.service';
+import {DataModelService} from '../DataModel/data-model.service';
+import {Subject} from 'rxjs';
+import {CurrencyType, GroupActivityType, GroupsType, UserType} from '../ServerCommunication/CommunicationInterface/parameterTypes';
+import {ObservableService} from '../ServerCommunication/CommunicationInterface/observable.service';
+import {Currency} from '../DataModel/Utils/Currency';
 
-describe('BasicDataUpdateService', () => {
-  let service: BasicDataUpdateService;
+
+/**
+ * This Integration test class tests BasicDataUpdateService and DataModelService while mocking ObservableService
+ */
+describe('BasicDataUpdateService and DataModel', () => {
+  let dataModel: DataModelService;
+  let updateService: BasicDataUpdateService;
+
+  const mockedObservableService = jasmine.createSpyObj('ObservableService',
+    ['getUserObservable', 'getGroupsObservable', 'getSettingsCurrencyObservable', 'getUserObservable']);
+  const mockedBalanceCalculatorService = jasmine.createSpyObj('BalanceCalculatorService', ['calculateBalances']);
+  const mockedGreedyOptimisationService = jasmine.createSpyObj('GreedyOptimisationService', ['calculateOptimisation']);
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(BasicDataUpdateService);
+    dataModel = new DataModelService(mockedBalanceCalculatorService, mockedGreedyOptimisationService);
+
+    const userObservable: Subject<UserType> = new Subject();
+    mockedObservableService.getUserObservable.and.returnValue(userObservable);
+
+    const groupsObservable: Subject<GroupsType> = new Subject();
+    mockedObservableService.getGroupsObservable.and.returnValue(groupsObservable);
+
+    const groupActivityObservable: Subject<GroupActivityType> = new Subject();
+    mockedObservableService.getGroupActivityObservable.and.returnValue(groupActivityObservable);
+
+    updateService = new BasicDataUpdateService(mockedObservableService, dataModel);
   });
 
+  /*
+
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(dataModel).toBeTruthy();
+    expect(updateService).toBeTruthy();
   });
+
+  it('should change currency', () => {
+    const mockedCurrencyLanguageObservable: Subject<CurrencyType> = new Subject();
+    mockedObservableService.getSettingsCurrencyObservable.and.returnValue(mockedCurrencyLanguageObservable);
+    mockedCurrencyLanguageObservable.next({currency: 'USD'});
+    expect (dataModel.user.currency).toEqual(Currency.USD);
+  });
+
+  */
 });
