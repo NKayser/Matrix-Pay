682cfec18c1ea44eb1fba6888dfd9f8591a3c611
Markus Rüggeberg <markus.rueggeberg@student.kit.edu>
2020-09-10 16:21:53 +0200

    deduplicated transactions in updateService. compiled build in dist folder(6)

--- src/app/Update/basic-data-update.service.ts
+++ src/app/Update/basic-data-update.service.ts
@@ -435,8 +435,11 @@ export class BasicDataUpdateService {
 
       const multipleTransactions: Transaction[] = [];
       for (const transactionType of param) {
-        const currentTransaction = this.updateSingleTransaction(transactionType);
-        multipleTransactions.push(currentTransaction);
+        if (this.dataModel.getGroup(transactionType.groupId) === null || this.dataModel.getGroup(transactionType.groupId).
+          getTransaction(transactionType.transactionId) === null){
+          const currentTransaction = this.updateSingleTransaction(transactionType);
+          multipleTransactions.push(currentTransaction);
+        }
       }
       const promise = this.dataModel.calculateBalances(param[0].groupId, multipleTransactions,
           param[param.length - 1].groupId);
